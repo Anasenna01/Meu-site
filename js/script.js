152429a293a827a365d22608c39cf207f0c5a3dc
Ana Senna <anapaulafreitasilva201272@gmail.com>
2023-12-21 19:13:06 -0300

    script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,9 @@
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
-    }
-}
+window.addEventListener("scroll", function(){
+    let header = this.document.querySelector('#header')
+    header.classList.toggle('rolagem', this.window.scrollY > 0)
+})
 
+//scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e){
         e.preventDefault();
